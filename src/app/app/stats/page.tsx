e0c6d5bdef71f7a667bb231e1d885ec479e2555f
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2024-08-16 18:04:11 +0200

    age, height and weight form setup

--- src/app/app/stats/page.tsx
+++ src/app/app/stats/page.tsx
@@ -1,58 +1,45 @@
-
 'use client'
 
-import { getStats } from "@/actions"
-import Bmr from "@/components/Bmr"
-import MeasurementsForm from "@/components/MeasurementsForm"
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Stats } from "@/types"
-import { useAuth } from "@clerk/nextjs"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import {useState, useEffect} from 'react'
-import { Form, useForm } from "react-hook-form"
-import { z } from "zod"
+import { FieldValues, useForm } from 'react-hook-form'
+import {z} from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
 
-const schema = z.object({
-  height: z.number(),
-  weight: z.number(),
-  age: z.number(),
-  gender: z.enum(['male', 'female'])
-})
-
-type StatsForm = z.infer<typeof schema>
-
-function onSubmit(values: StatsForm){
-  console.log(values)
-}
-
-export default function StatsPage() {
-  const {userId} = useAuth()
-  const [stats, setStats] = useState<Stats>()
-  const form = useForm<StatsForm>({
-    resolver: zodResolver(schema)
+const statsSchema = z.object({
+    age: z.number().optional(),
+    height: z.number().optional(),
+    weight: z.number().optional(),
+    
   })
+
   
-  useEffect(() => {
-    updateStats()
-  }, [])
+  type StatsForm = z.infer<typeof statsSchema>
   
-  async function updateStats(){
-    const stats = await getStats(userId!)
-    console.log(stats)
-    setStats(stats)
+  function onSubmit(values: StatsForm){
+    console.log(values)
   }
 
-  
+export default function MeasurementsForm(){
+    const form = useForm<StatsForm>({
+        resolver: zodResolver(statsSchema)
+      })
+      
+    return (
+      <div className="page-container">
 
-  
-
-  return (
-    <main className="page-container overflow-auto">
-      <h1>StatsPage</h1>
         <Form {...form}>
-      <form>
-      <FormField
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
             key={'age'}
               control={form.control}
               name={'age'}
@@ -66,11 +53,37 @@ export default function StatsPage() {
                 </FormItem>
               )}
             />
-      </form>
-        </Form>
-      {/* <p>{stats.height}</p> */}
-      {/* <Bmr/>
-      <MeasurementsForm/> */}
-    </main>
-  )
+            <FormField
+            key={'height'}
+              control={form.control}
+              name={'height'}
+              render={({field}) => (
+                <FormItem>
+                <FormLabel>{'Height'}</FormLabel>
+                <FormControl>
+                  <Input {...field}/>
+                </FormControl>
+                <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <FormField
+            key={'weight'}
+              control={form.control}
+              name={'weight'}
+              render={({field}) => (
+                <FormItem>
+                <FormLabel>{'Weight'}</FormLabel>
+                <FormControl>
+                  <Input {...field}/>
+                </FormControl>
+                <FormMessage/>
+                </FormItem>
+              )}
+            />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+              </div>
+    )
 }
