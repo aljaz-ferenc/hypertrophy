a2e2a5fd7edee90fb3eb69c49e6ca999de98afd5
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2024-10-05 19:07:08 +0200

    remove cors

--- src/runMiddleware.ts
+++ src/runMiddleware.ts
@@ -1,20 +1,20 @@
-import Cors from 'cors';
+// import Cors from 'cors';
 
-// Initialize the cors middleware with your app's origin
-const cors = Cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  origin: 'http://localhost:8081', // Adjust this to match your app's domain
-});
+// // Initialize the cors middleware with your app's origin
+// const cors = Cors({
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   origin: 'http://localhost:8081', // Adjust this to match your app's domain
+// });
 
-export function runMiddleware(req: any, res: any, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
+// export function runMiddleware(req: any, res: any, fn: any) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+//       return resolve(result);
+//     });
+//   });
+// }
 
-export default cors;
+// export default cors;
