10de8937bc800ddbbab2b50d810d81ac919d5a1a
Aljaž Ferenc <aljaz.ferenc@specto.si>
2024-10-28 15:34:17 +0100

    check password length and match

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,9 +1,26 @@
 import {NextResponse} from "next/server";
 
+type RequestBody = {
+    username: string,
+    password: string,
+    passwordConfirm: string
+}
+
 export async function POST(request: Request) {
     try {
-        const body = await request.json()
-        return NextResponse.json(body);
+        const {username, password, passwordConfirm}: RequestBody = await request.json()
+
+        const passwordsMatch = password === passwordConfirm
+        if (!passwordsMatch) {
+            return NextResponse.json({message: 'Passwords do not match', status: 400},)
+        }
+
+        const isPasswordLongEnough = password.length > 5
+        if (!isPasswordLongEnough) {
+            return NextResponse.json({message: 'Passwords must be at least 6 characters long', status: 400},)
+        }
+
+        return NextResponse.json('');
     } catch (err: unknown) {
         if (err instanceof Error) {
             console.log(err.message)
@@ -13,7 +30,7 @@ export async function POST(request: Request) {
 
 export async function OPTIONS() {
     return NextResponse.json(
-        { message: "CORS preflight response" },
+        {message: "CORS preflight response"},
         {
             headers: {
                 "Access-Control-Allow-Origin": "*",
