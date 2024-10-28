dd8518f5d688816beb400751c91d7d52074b82a5
Aljaž Ferenc <aljaz.ferenc@specto.si>
2024-10-28 15:27:12 +0100

    login and register endpoints setup

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,26 @@
+import {NextResponse} from "next/server";
+
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json()
+        return NextResponse.json(body);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.log(err.message)
+        }
+    }
+}
+
+export async function OPTIONS() {
+    return NextResponse.json(
+        { message: "CORS preflight response" },
+        {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+        }
+    );
+}
