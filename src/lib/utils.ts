db1bb060af70fb7e6602c3cbc5987c54dcd93f54
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-11-26 23:16:40 +0100

    type error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -69,7 +69,7 @@ export const getAverageNutrition = (nutrition: Array<any>) => {
     }, {calories: 0, protein: 0, fat: 0, carbs: 0})
 }
 
-export function groupBy(array, keyGetter) {
+export function groupBy(array: any[], keyGetter: any) {
     return array.reduce((result, currentItem) => {
         const key = keyGetter(currentItem);
         if (!result[key]) {
